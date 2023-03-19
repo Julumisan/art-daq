3046284b5f25c2eed7bfe7b1431c2610064ae18e
Julumisan <91879316+Julumisan@users.noreply.github.com>
2023-03-19 12:46:09 +0100

    Cambio de versión

--- setup.py
+++ setup.py
@@ -10,7 +10,7 @@ from setuptools import setup
 
 setup(
     name='art-daq',
-    version='0.1.0',
+    version='0.1.1',
     description='Paquete para usar la tarjeta de NI, USB-6001',
     packages=['Script'],
     install_requires=[
