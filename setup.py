38eb42fd6599ff2b74359d1d7493b34a9a6625c1
Julumisan <91879316+Julumisan@users.noreply.github.com>
2023-03-27 09:34:24 +0200

    v1.0.2

--- setup.py
+++ setup.py
@@ -10,12 +10,11 @@ from setuptools import setup
 
 setup(
     name='art-daq',
-    version='1.0.1',
+    version='1.0.2',
     description='Paquete para usar la tarjeta de NI, USB-6001',
     packages=['art_daq'],
     install_requires=[
         'nidaqmx',
-        'math',
         'tkinter',
         'matplotlib',
         'numpy'
