622590a84bac0ccae0c5017b3074f08cd13a682a
Julumisan <91879316+Julumisan@users.noreply.github.com>
2023-08-11 09:05:30 +0200

    Version Final 2.4.1

--- setup.py
+++ setup.py
@@ -10,7 +10,7 @@ from setuptools import setup
 
 setup(
     name='art-daq',
-    version='2.0.1',
+    version='2.2.1',
     description='Paquete para usar la tarjeta de NI, USB-6001',
     packages=['art_daq'],
     install_requires=[
